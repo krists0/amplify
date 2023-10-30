07cfebee5dd493bd6641142365b8e8304a0a0cff
Kristina Tsivilov <christina0147@gmail.com>
2023-10-30 12:29:33 +0200

    fix3

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React app
+          Learn React app jhj
         </a>
       </header>
     </div>
