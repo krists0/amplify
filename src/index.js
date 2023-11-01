c5fc11f7c2ecca0f74d4255301f00c8fe8d69efe
Kristina Tsivilov <christina0147@gmail.com>
2023-11-01 10:02:54 +0200

    changes2

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Amplify } from 'aws-amplify';
-import awsExports from '../src/aws-exports'
+import awsExports from '../src/aws-exports.js';
 Amplify.configure(awsExports);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
